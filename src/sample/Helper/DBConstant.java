67ff0c45fb3e1beac7a43c7f5478ba5a39f9daa9
ThanhTran <acerhoa5@gmail.com>
2019-11-10 21:49:46 -0600

    Fixed: stf_lname error

--- src/sample/Helper/DBConstant.java
+++ src/sample/Helper/DBConstant.java
@@ -4,7 +4,7 @@ public class DBConstant {
     public static final String STAFF_TABLE = "Staff";
 
     public static final String STAFF_FIRSTNAME = "stf_fname";
-    public static final String STAFF_LASTNAME = "stf_lname";
+    public static final String STAFF_LASTNAME = "sft_lname";
     public static final String STAFF_USERNAME = "stf_username";
     public static final String STAFF_PASSWORD = "stf_password";
     public static final String STAFF_PHONE = "stf_phone";
